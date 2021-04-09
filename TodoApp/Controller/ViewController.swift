f6e67586638cbf19586064704b939fd805c11633
kunihei <57933493+kunihei@users.noreply.github.com>
2021-04-10 08:16:15 +0900

    todolistの登録機能を実装

--- TodoApp/Controller/ViewController.swift
+++ TodoApp/Controller/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var date: UIDatePicker!
+    @IBOutlet weak var textField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touroku(_ sender: Any) {
+        if textField.text.isEmpty != true{
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy年MM月dd日"
+            
+            UserDefaults.standard.setValue(textField.text, forKey: "comment")
+            UserDefaults.standard.setValue(formatter.string(from: date.date), forKey: "date")
+        }
+    }
 }
 
